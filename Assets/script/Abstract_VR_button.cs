c9ec13c76ecb30db715d7971217727c470c3855b
Fabio Vitello <fvitello@users.noreply.github.com>
2022-04-01 16:17:27 +0200

    Update Abstract_VR_button.cs

--- Assets/script/Abstract_VR_button.cs
+++ Assets/script/Abstract_VR_button.cs
@@ -4,8 +4,8 @@
  *                https://www.geavr.eu/
  *             https://github.com/GeaVR/GeaVR
  * 
- * GeaVR is an open source software that allows the user to experience a wide range 
- * of geological and geomorphological sites in virtual reality.
+ * GeaVR is an open source software that allows the user to experience a wide 
+ * range of geological and geomorphological sites in immersive virtual reality.
  *
  * Main Developers:      
  * 
@@ -16,14 +16,15 @@
  * Developed thanks to the contribution of following projects:
  *
  *     ACPR15T4_ 00098 “Agreement between the University of Milan Bicocca and the 
- *     Cometa Consortium for the experimentation of cutting-edge interactive technologies
- *     for the improvement of science teaching and dissemination” of 
+ *     Cometa Consortium for the experimentation of cutting-edge interactive 
+ *     technologies for the improvement of science teaching and dissemination” of 
  *     Italian Ministry of Education, University and Research (ARGO3D)
  *     PI: Alessandro Tibaldi (alessandro.tibaldi@unimib.it)
  *     
- *     Erasmus+ Key Action 2 2017-1-UK01-KA203- 036719 “3DTeLC – Bringing the 3D-world 
- *     into the classroom: a new approach to Teaching, Learning and Communicating the 
- *     science of geohazards in terrestrial and marine environments”
+ *     Erasmus+ Key Action 2 2017-1-UK01-KA203- 036719 “3DTeLC – Bringing the  
+ *     3D-world into the classroom: a new approach to Teaching, Learning and 
+ *     Communicating the science of geohazards in terrestrial and marine 
+ *     environments”
  *     PI: Malcolm Whitworth (malcolm.Whitworth@port.ac.uk)
  * 
  ******************************************************************************
