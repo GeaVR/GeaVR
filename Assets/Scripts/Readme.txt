074d6f12b96f34bdac953e071350ace894d5ecd3
Fabio Vitello <fvitello@users.noreply.github.com>
2022-04-07 10:13:05 +0200

    Removed old files and  added header on top of code

--- Assets/Scripts/Readme.txt
+++
@@ -1,5 +0,0 @@
- - PositionController.cs va aggiunto a FPSController. A FPSController va inoltre aggiunto un collider trigger;
- - TileData.cs va aggiunto a tutte le Tile;
- - TileObject non va aggiunto a niente.
-
- La cartella Tiles va messa nella cartella Assets del progetto.
--- Assets/Scripts/Tools/ToolInstance.cs
+++ Assets/Scripts/Tools/ToolInstance.cs
@@ -1,4 +1,39 @@
-﻿using System.Collections;
+﻿/******************************************************************************
+ *
+ *                      GeaVR
+ *                https://www.geavr.eu/
+ *             https://github.com/GeaVR/GeaVR
+ * 
+ * GeaVR is an open source software that allows the user to experience a wide 
+ * range of geological and geomorphological sites in immersive virtual reality,
+ * including data collection.
+ *
+ * Main Developers:      
+ * 
+ *     Fabio Luca Bonali (fabio.bonali@unimib.it)
+ *     Martin Kearl (martintkearl@gmail.com)
+ *     Fabio Roberto Vitello (fabio.vitello@inaf.it)
+ * 
+ * Developed thanks to the contribution of following projects:
+ *
+ *     ACPR15T4_ 00098 “Agreement between the University of Milan Bicocca and the 
+ *     Cometa Consortium for the experimentation of cutting-edge interactive 
+ *     technologies for the improvement of science teaching and dissemination” of 
+ *     Italian Ministry of Education, University and Research (ARGO3D)
+ *     PI: Alessandro Tibaldi (alessandro.tibaldi@unimib.it)
+ *     
+ *     Erasmus+ Key Action 2 2017-1-UK01-KA203- 036719 “3DTeLC – Bringing the  
+ *     3D-world into the classroom: a new approach to Teaching, Learning and 
+ *     Communicating the science of geohazards in terrestrial and marine 
+ *     environments”
+ *     PI: Malcolm Whitworth (malcolm.Whitworth@port.ac.uk)
+ * 
+ ******************************************************************************
+ * Copyright (c) 2016-2022
+ * GPL-3.0 License
+ *****************************************************************************/
+
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System;
